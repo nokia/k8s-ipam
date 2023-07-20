ce436d03cdbeac731511adcad865cef7d498cfb8
Wim Henderickx <wim.henderickx@gmail.com>
2023-07-20 12:47:38 +0200

    go fmt

--- main.go
+++ main.go
@@ -36,10 +36,10 @@ import (
 	_ "github.com/nokia/k8s-ipam/controllers/ipclaim"
 	_ "github.com/nokia/k8s-ipam/controllers/ipnetworkinstance"
 	_ "github.com/nokia/k8s-ipam/controllers/ipprefix"
+	_ "github.com/nokia/k8s-ipam/controllers/node"
 	_ "github.com/nokia/k8s-ipam/controllers/rawtopology"
 	_ "github.com/nokia/k8s-ipam/controllers/vlanclaim"
 	_ "github.com/nokia/k8s-ipam/controllers/vlanindex"
-	_ "github.com/nokia/k8s-ipam/controllers/node"
 
 	//_ "github.com/nokia/k8s-ipam/controllers/vlanspecializer"
 	_ "github.com/nokia/k8s-ipam/controllers/vlanvlan"
